1d74da52e75e70d8f071152c791a824422d1d437
lingy12 <geyulin.lin@gmail.com>
2022-06-12 14:29:48 +0800

    add unet model

--- models.py
+++ models.py
@@ -22,7 +22,6 @@ class Model:
             model_name (str): name of the model.
             log (bool, optional): log or not. Defaults to False.
         """
-        # TODO: log not supported
         self.model = None
         self.model_name = model_name
         self.log = log
@@ -52,7 +51,77 @@ class Model:
         model.add(Dense(feature_num, activation='relu'))
         model.add(Dense(output_size, activation='softmax'))
         self.model = model
+    
+    def conv1d_block(input_tensor, n_filters, kernel_size = 3, batchnorm = True):
+        """Function to add 2 convolutional layers with the parameters passed to it"""
+        # first layer
+        x = Conv1D(filters = n_filters, kernel_size = kernel_size,\
+                kernel_initializer = 'he_normal', padding = 'same')(input_tensor)
+        if batchnorm:
+            x = BatchNormalization()(x)
+        x = Activation('relu')(x)
         
+        # second layer
+        x = Conv1D(filters = n_filters, kernel_size = kernel_size,\
+                kernel_initializer = 'he_normal', padding = 'same')(x)
+        if batchnorm:
+            x = BatchNormalization()(x)
+        x = Activation('relu')(x)
+        
+        return x
+    
+    def get_unet(input_imu, output_unit = -1, for_segamentation = True, n_filters = 16, dropout = 0.1, batchnorm = True):
+        """Function to define the UNET Model"""
+        # Contracting Path
+        c1 = conv1d_block(input_imu, n_filters * 1, kernel_size = 3, batchnorm = batchnorm)
+        p1 = MaxPooling1D(2)(c1)
+        p1 = Dropout(dropout)(p1)
+        
+        c2 = conv1d_block(p1, n_filters * 2, kernel_size = 3, batchnorm = batchnorm)
+        p2 = MaxPooling1D(2)(c2)
+        p2 = Dropout(dropout)(p2)
+        
+        c3 = conv1d_block(p2, n_filters * 4, kernel_size = 3, batchnorm = batchnorm)
+        p3 = MaxPooling1D(2)(c3)
+        p3 = Dropout(dropout)(p3)
+        
+        c4 = conv1d_block(p3, n_filters * 8, kernel_size = 3, batchnorm = batchnorm)
+        p4 = MaxPooling1D(2)(c4)
+        p4 = Dropout(dropout)(p4)
+        
+        c5 = conv1d_block(p4, n_filters = n_filters * 16, kernel_size = 3, batchnorm = batchnorm)
+        
+        # Expansive Path
+        u6 = Conv1DTranspose(n_filters * 8, 3, strides = 2, padding = 'same')(c5)
+        u6 = concatenate([u6, c4])
+        u6 = Dropout(dropout)(u6)
+        c6 = conv1d_block(u6, n_filters * 8, kernel_size = 3, batchnorm = batchnorm)
+        
+        u7 = Conv1DTranspose(n_filters * 4, 3, strides = 2, padding = 'same')(c6)
+        u7 = concatenate([u7, c3])
+        u7 = Dropout(dropout)(u7)
+        c7 = conv1d_block(u7, n_filters * 4, kernel_size = 3, batchnorm = batchnorm)
+        
+        u8 = Conv1DTranspose(n_filters * 2, 3, strides = 2, padding = 'same')(c7)
+        u8 = concatenate([u8, c2])
+        u8 = Dropout(dropout)(u8)
+        c8 = conv1d_block(u8, n_filters * 2, kernel_size = 3, batchnorm = batchnorm)
+        
+        u9 = Conv1DTranspose(n_filters * 1, 3, strides = 2, padding = 'same')(c8)
+        u9 = concatenate([u9, c1])
+        u9 = Dropout(dropout)(u9)
+        c9 = conv1d_block(u9, n_filters * 1, kernel_size = 3, batchnorm = batchnorm)
+        
+        outputs = Conv1D(1, 1, activation='sigmoid')(c9)
+        
+        if not for_segamentation:
+            outputs = Dense(output_unit)
+        model = Model(inputs=[input_imu], outputs=[outputs])
+        
+    #     model = Model(inputs=[input_imu], outputs=[c5])
+        
+        return model
+    
     def fit(self, x, y, **kwargs):
         """Fit the model using tensorflow fit function.
         """

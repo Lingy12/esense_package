9ffef5d80d346501c78daed742a2d6e594b2de27
Lingy12 <geyulin.lin@gmail.com>
2022-06-21 18:51:27 +0800

    update logic

--- data_tool.py
+++ data_tool.py
@@ -335,7 +335,7 @@ class DataGenerator:
                     continue
                 
                 # Ensure the overlapping of touching point
-                if label_pattern == 1 or (data_start < touch_touching_point - data_length or data_end < touch_touching_point):
+                if label_pattern == 1 and (data_start < touch_touching_point - data_length or data_end < touch_touching_point):
                     # print('Skipping non-overlapping')
                     continue
                 

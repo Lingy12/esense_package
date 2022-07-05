10d15b7afe1fed4bbdd880aa81216ee01fa04d6d
Lingy12 <geyulin.lin@gmail.com>
2022-07-05 13:06:27 +0800

    add forcasting model

--- models.py
+++ models.py
@@ -37,6 +37,20 @@ class ClassificationModel(tf.keras.Model):
     
     def call(self, inputs):
         return self.layer(inputs)
+
+class ForcastingCNNModel(tf.keras.Model):
+        def __init__(self, filters_num:int, kernel_size:int, input_shape:int, forcasting_length:int, 
+                                        feature_num: int = 100, dropout_rate:int = 0.5, pool_size:int=2, 
+                                        regularize_ratio:float = 0):
+            super().__init__()
+            self.layer = Conv1DBlock(filters_num, kernel_size, input_shape, forcasting_length * 6, 
+                                     feature_num, dropout_rate, pool_size, regularize_ratio, True, 'ConvLayer')
+            self.reshape_layer = tf.keras.layers.Reshape((-1, 6))
+
+        def call(self, inputs):
+            x = self.layer(inputs)
+            
+            return self.reshape_layer(x)
     
 class UNetSegmentationModel(tf.keras.Model):
     def __init__(self):
